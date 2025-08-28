5b36d5db8c540c01b5a104cea2c0c354a393c009
Xxeet7 <2good4u991@gmail.com>
2025-08-28 14:17:07 +0700

    feat: add support for wsl

--- src/Clipboard.php
+++ src/Clipboard.php
@@ -1,127 +1,132 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Clipboard - perform clipboard operations in PHP
  * Homepage: https://github.com/asvvvad/clipboard
  * Made by ASVVVAD (https://asvvad.eu.org)
  * Forked and maintained by Xxeet7 (https://github.com/Xxeet7/clipboard.php)
  */
-class Clipboard
+final class Clipboard
 {
-	/**
-	 * @access private
-	 * @var bool $unsupported
-	 */
-	private $unsupported = false;
+    /**
+     * @var bool
+     */
+    private $unsupported = false;
 
-	/**
-	 * @access private
-	 * @var string $copyCmdArgs
-	 */
-	private $copyCmdArgs;
+    /**
+     * @var string
+     */
+    private $copyCmdArgs;
 
-	/**
-	 * @access private
-	 * @var string $pasteCmdArgs
-	 */
-	private $pasteCmdArgs;
+    /**
+     * @var string
+     */
+    private $pasteCmdArgs;
 
-	/**
-	 * Decide which method to use based on OS and availability
-	 */
-	function __construct()
-	{
-		switch (PHP_OS_FAMILY) {
-			case 'Solaris': case 'BSD': case 'Linux':
-				if (getenv('WAYLAND_DISPLAY') != false) {
-					if ($this->lookPath('wl-paste') and $this->lookPath('wl-copy')) {
-						$this->pasteCmdArgs = "wl-paste --no-newline";
-						$this->copyCmdArgs = "wl-copy";
-						break;
-					} // fall back to xclip or xsel
-				}
-				if ($this->lookPath('xclip')) {
-					$this->pasteCmdArgs = 'xclip -out -selection clipboard';
-					$this->copyCmdArgs = 'xclip -in -selection clipboard';
-				} elseif ($this->lookPath('xsel')) {
-					$this->pasteCmdArgs = 'xsel --output --clipboard';
-					$this->copyCmdArgs = 'xsel --input --clipboard';
-				} elseif ($this->lookPath('termux-clipboard-get') and $this->lookPath('termux-clipboard-get')) {
-					$this->pasteCmdArgs = 'termux-clipboard-get';
-					$this->copyCmdArgs = 'termux-clipboard-set';
-				} else {
-					$this->unsupported = true;
-				}
-				break;
-			case 'Darwin':
-				$this->pasteCmdArgs = 'pbpaste';
-				$this->copyCmdArgs = 'pbcopy';
-				break;
-			case 'Windows':
-				// needs: https://www.c3scripts.com/tutorials/msdos/paste.html#exe
-        if ($this->lookPath("paste.exe")) {
-					$this->pasteCmdArgs = 'paste';
-				} else {
-					// https://github.com/Microsoft/WSL/issues/1069
-					// slower
-					$this->pasteCmdArgs = 'powershell.exe -Command Get-Clipboard';
-				}
-				$this->copyCmdArgs = 'clip';
-				break;
-			default:
-				$this->unsupported = true;
-				break;
-		}
-	}
+    /**
+     * Decide which method to use based on OS and availability
+     */
+    public function __construct()
+    {
+        switch (PHP_OS_FAMILY) {
+            case 'Solaris': case 'BSD': case 'Linux':
+                if (getenv('WAYLAND_DISPLAY') !== false) {
+                    if ($this->lookPath('wl-paste') and $this->lookPath('wl-copy')) {
+                        $this->pasteCmdArgs = 'wl-paste --no-newline';
+                        $this->copyCmdArgs = 'wl-copy';
+                        break;
+                    } // fall back to xclip or xsel
+                }
+                if ($this->lookPath('xclip')) {
+                    $this->pasteCmdArgs = 'xclip -out -selection clipboard';
+                    $this->copyCmdArgs = 'xclip -in -selection clipboard';
+                } elseif ($this->lookPath('xsel')) {
+                    $this->pasteCmdArgs = 'xsel --output --clipboard';
+                    $this->copyCmdArgs = 'xsel --input --clipboard';
+                } elseif ($this->isWSL()) {
+                    $this->copyCmdArgs = 'clip.exe';
+                    $this->pasteCmdArgs = 'powershell.exe -Command Get-Clipboard';
+                } elseif ($this->lookPath('termux-clipboard-get') and $this->lookPath('termux-clipboard-get')) {
+                    $this->pasteCmdArgs = 'termux-clipboard-get';
+                    $this->copyCmdArgs = 'termux-clipboard-set';
+                } else {
+                    $this->unsupported = true;
+                }
+                break;
+            case 'Darwin':
+                $this->pasteCmdArgs = 'pbpaste';
+                $this->copyCmdArgs = 'pbcopy';
+                break;
+            case 'Windows':
+                // needs: https://www.c3scripts.com/tutorials/msdos/paste.html#exe
+                if ($this->lookPath('paste.exe')) {
+                    $this->pasteCmdArgs = 'paste';
+                } else {
+                    // https://github.com/Microsoft/WSL/issues/1069
+                    // slower
+                    $this->pasteCmdArgs = 'powershell.exe -Command Get-Clipboard';
+                }
+                $this->copyCmdArgs = 'clip';
+                break;
+            default:
+                $this->unsupported = true;
+                break;
+        }
+    }
 
-	/**
-	 * Read clipboard content
-	 * @return string
-	 */
-	public function readAll(): string
-	{
-		return exec($this->pasteCmdArgs);
-	}
+    /**
+     * Read clipboard content
+     */
+    public function readAll(): string
+    {
+        return exec($this->pasteCmdArgs);
+    }
 
+    /**
+     * Write string to clipboard
+     */
+    public function writeAll($text): bool
+    {
+        $process = popen($this->copyCmdArgs, 'wb');
 
-	/**
-	 * Write string to clipboard
-	 * @param $text
-	 * @param
-	 */
-	public function writeAll($text): bool
-	{
-		$process = popen($this->copyCmdArgs, "wb");
+        if (is_resource($process)) {
+            $w = fwrite($process, (string) $text);
+            pclose($process);
+        }
 
-		if (is_resource($process)) {
-			$w = fwrite($process, (string)$text);
-			pclose($process);
-		}
-		return $text == $w;
-	}
+        return $text === $w;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isUnsupported(): bool
-	{
-		return $this->unsupported;
-	}
+    public function isUnsupported(): bool
+    {
+        return $this->unsupported;
+    }
 
+    private function isWSL(): bool
+    {
+        if (PHP_OS_FAMILY !== 'Linux') {
+            return false;
+        }
+        $release = mb_strtolower(php_uname('r'));
 
-	/**
-	 * Look in PATH for an excutable
-	 * @param string $excutable what to look for
-	 * @return bool
-	 */
-	private function lookPath(string $excutable): bool
-	{
-		foreach (explode(PATH_SEPARATOR, getenv('PATH')) as $dir) {
-			if (is_dir($dir) and in_array($excutable, scandir($dir))) {
-				return true;
-			}
-		}
-		return false;
-	}
+        return mb_strpos($release, 'microsoft') !== false || mb_strpos($release, 'wsl') !== false;
+    }
 
+    /**
+     * Look in PATH for an excutable
+     *
+     * @param  string  $excutable  what to look for
+     */
+    private function lookPath(string $excutable): bool
+    {
+        foreach (explode(PATH_SEPARATOR, getenv('PATH')) as $dir) {
+            if (is_dir($dir) and in_array($excutable, scandir($dir))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
